1d6785dcd78b469c05e3edb6a42d339bb93f4fad
Suraj-1408 <surajpaswanc0497@gmail.com>
2026-01-03 09:28:49 +0530

    Resolved merged conflict- removed env logs

--- app/api/pastes/route.ts
+++ app/api/pastes/route.ts
@@ -3,12 +3,6 @@ import pool from "@/lib/db";
 
 export async function POST(req: Request) {
   console.log("Inside Paste bin API");
-  console.log('DB_USER:', process.env.DB_USER); 
-  console.log('DB_PORT:', process.env.DB_PORT);
-  console.log('DB_NAME:', process.env.DB_NAME);
-  console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '[HIDDEN]' : 'MISSING');
-  console.log('DB_SSL:', process.env.DB_SSL);
-  console.log('====================');
 
   //printing db values
   console.log("DB HOST:", process.env.DB_HOST);
